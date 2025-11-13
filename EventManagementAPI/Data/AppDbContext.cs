e99c67fb2400066ad5e2bcb77746afb6638b605f
Harvi <harvitandel7@gmail.com>
2025-11-13 12:35:14 +0530

    Update AppDbContext.cs

--- EventManagementAPI/Data/AppDbContext.cs
+++ EventManagementAPI/Data/AppDbContext.cs
@@ -1,12 +1,31 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using EventManagementAPI.Models;
 
 namespace EventManagementAPI.Data
 {
-    public class AppDbContext:DbContext
+    public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+           : base(options)
+        {
+        }
 
-         public DbSet<Event> Events { get; set; }
+        public DbSet<Event> Events { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Achievement> Achievements { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // ✅ Convert Enum → String in database
+            modelBuilder
+                .Entity<Achievement>()
+                .Property(a => a.Category)
+                .HasConversion<string>();
+
+            base.OnModelCreating(modelBuilder);
+
+        }
     }
 }
+
