1bf5fc2e744d06399f16cc213bbbf05e4a9fffbe
Harvi <harvitandel7@gmail.com>
2025-11-17 13:38:22 +0530

    Add files via upload

---
+++ EventManagementAPI/Models/Gallery.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventManagementAPI.Models
+
+{
+    public class Gallery
+
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public string? Category { get; set; }
+
+        public bool IsActive { get; set; } = true;
+        public bool IsDeleted { get; set; } = false;
+
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime Updated { get; set; } = DateTime.Now;
+
+        public List<GalleryImage> Images { get; set; } = new List<GalleryImage>();
+    }
+}
+
---
+++ EventManagementAPI/Models/GalleryImage.cs
@@ -0,0 +1,11 @@
+﻿namespace EventManagementAPI.Models
+{
+    public class GalleryImage
+    {
+        public int Id { get; set; }
+        public string ImageUrl { get; set; }
+        public int GalleryId { get; set; }
+
+        public Gallery Gallery { get; set; }
+    }
+}
