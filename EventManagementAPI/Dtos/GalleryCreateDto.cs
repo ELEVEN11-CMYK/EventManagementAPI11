ffee40cdf451f3284112e3a09dcc76195b09667a
Harvi <harvitandel7@gmail.com>
2025-11-17 13:38:56 +0530

    Add files via upload

---
+++ EventManagementAPI/Dtos/GalleryCreateDto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace EventManagementAPI.Dtos
+{
+    public class GalleryCreateDto
+    {
+        [Required]
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public string? Category { get; set; }
+
+        [Required]
+        public IFormFile[] Images { get; set; }
+    }
+}
---
+++ EventManagementAPI/Dtos/GalleryUpdateDto.cs
@@ -0,0 +1,13 @@
+﻿namespace EventManagementAPI.Dtos
+{
+    public class GalleryUpdateDto
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? Category { get; set; }
+        public bool? IsActive { get; set; }
+
+        public IFormFile[]? NewImages { get; set; }
+        public int[]? DeleteImageIds { get; set; }
+    }
+}
