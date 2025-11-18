0e8db4d9cd47e001d5cbd87314056f7bdb0bdb90
Harvi <harvitandel7@gmail.com>
2025-11-18 17:39:49 +0530

    Add files via upload

---
+++ EventManagementAPI/Dtos/FacilityCreateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace EventManagementAPI.Dtos
+{
+    public class FacilityCreateDto
+    {
+        public string FacilityName { get; set; }
+        public string Description { get; set; }
+        public string Icon { get; set; }
+        public string ColorTheme { get; set; }
+    }
+}
