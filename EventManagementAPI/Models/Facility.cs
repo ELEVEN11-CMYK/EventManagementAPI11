4829048aebf37c227a52c8d1f6ebb3407922b5a1
Harvi <harvitandel7@gmail.com>
2025-11-18 17:39:18 +0530

    Add files via upload

---
+++ EventManagementAPI/Models/Facility.cs
@@ -0,0 +1,16 @@
+﻿namespace EventManagementAPI.Models
+{
+    public class Facility
+    {
+        public int Id { get; set; }
+        public string FacilityName { get; set; }
+        public string Description { get; set; }
+        public string Icon { get; set; }
+        public string ColorTheme { get; set; }
+        public bool IsActive { get; set; } = true;
+        public bool IsDeleted { get; set; } = false;
+
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime Updated { get; set; } = DateTime.Now;
+    }
+}
