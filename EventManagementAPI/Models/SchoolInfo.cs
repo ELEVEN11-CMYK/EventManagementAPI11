5567f2bf1db871f4f1dceb68c96a0111efa2cb17
Harvi <harvitandel7@gmail.com>
2025-11-20 14:48:46 +0530

    Add files via upload

---
+++ EventManagementAPI/Models/SchoolInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EventManagementAPI.Models
+{
+    public class SchoolInfo
+    {
+        public int Id { get; set; }
+        public string? SchoolName { get; set; }
+        public string? Address { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? LogoUrl { get; set; }
+        public string? About { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
