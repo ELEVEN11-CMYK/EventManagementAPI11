2ac912a22e0327c75f12d29a9157b96b013035f6
Harvi <harvitandel7@gmail.com>
2025-11-20 14:46:57 +0530

    Add files via upload

---
+++ EventManagementAPI/Dtos/SchoolInfoPatchDto.cs
@@ -0,0 +1,12 @@
+﻿namespace EventManagementAPI.Dtos
+{
+    public class SchoolInfoPatchDto
+    {
+        public string? SchoolName { get; set; }
+        public string? Address { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? LogoUrl { get; set; }
+        public string? About { get; set; }
+    }
+}
